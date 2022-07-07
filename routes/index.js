e6da6d5767d86222d5592da8ee7ae2ec2870f63d
Elena <elli_shii@mail.ru>
2022-07-07 21:46:46 +0300

    add StartPage jsx

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
-const Home = require('../views/Home');
-const Reg  = require('../views/Reg');
+const StartPage = require('../views/StartPage');
+// const Reg = require('../views/Reg');
 
 router.get('/', async (req, res) => {
-  res.renderComponent(Home, { title:'Home page' });
+  res.renderComponent(StartPage, { title: 'StartPage' });
 });
 
 module.exports = router;
