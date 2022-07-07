5167caeff1a01e268bea112e62550eabc6d7d244
Elena <elli_shii@mail.ru>
2022-07-07 16:36:10 +0300

    add style reg

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -7,11 +7,10 @@ module.exports = function Layout({ children, title }) {
 
       <head>
         <meta charSet="UTF-8" />
-        {/*<link rel="stylesheet" href="/css/index.css" /> */}
         {/* <script defer src="/js/application.js" /> */}
         <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-          rel="stylesheet"
+          // href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+          // rel="stylesheet"
           integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
           crossorigin="anonymous"
         />
@@ -20,6 +19,7 @@ module.exports = function Layout({ children, title }) {
           integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
           crossorigin="anonymous"
         />
+        <link rel="stylesheet" href="css/style.css" />
         <title>{title}</title>
       </head>
 
--- views/Reg.jsx
+++ views/Reg.jsx
@@ -20,7 +20,7 @@ module.exports = function Reg({ errors }) {
           </div>
         )}
 
-        <div className="md-3 shadow bg-white rounded">
+        {/* <div className="md-3 shadow bg-white rounded">
           <h1 className='font-monospace'>Регистрация</h1>
           <form method="post" action="/reg">
             <label htmlFor="name_input">Имя:</label>
@@ -44,7 +44,36 @@ module.exports = function Reg({ errors }) {
             <input type="submit" value="Зарегистрироваться" className="btn btn-primary" />
           </form>
         </div>
+      </main> */}
+        <div className="reg">
+          {/* <h1 className='font-monospace'>Регистрация</h1> */}
+          <form className='reg_form' method="post" action="/reg">
+            <h1 className='reg_content'>Регистрация</h1>
+            <br />
+            <label className='reg_content reg_text' htmlFor="name_input">Имя:</label>
+            <br />
+            <input className='reg_content reg_input' id="name_input" name="user[name]" type="text" value="" />
+
+            <div className="form-check">
+              <label className="reg_content reg_text" for="flexRadioDefault1">Бабушка: </label>
+              <input className="reg_content reg_input_radio" type="radio" name="user[role]" value="gm" id="flexRadioDefault1" checked />
+            </div>
+
+            <div className="form-check">
+              <label className="reg_content reg_text" for="flexRadioDefault2">Внук: </label>
+              <input className="reg_content reg_input_radio" type="radio" name="user[role]" value="gs" id="flexRadioDefault2" />
+            </div>
+
+            <label className="reg_content reg_text" htmlFor="password_input">Пароль:</label>
+            <br />
+            <input className="reg_content reg_input" id="password_input" name="user[password]" type="password" value="" />
+            <br />
+            <button className="reg_content btn btn-primary" type="submit" value="Зарегистрироваться">Зарегистрироваться</button>
+            {/* <input className="reg_content btn btn-primary" type="submit" value="Зарегистрироваться" /> */}
+          </form>
+        </div>
       </main>
+
 
     </Layout>
   );
