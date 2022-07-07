b5771569955a76283450ebc9235552723c3f2032
Nikita Krivosheyev <nik.krivosheyev@gmail.com>
2022-07-08 00:55:33 +0300

    Card and CardList views added

---
+++ views/Card.jsx
@@ -0,0 +1,18 @@
+const React = require('react');
+
+module.exports = function Card({ card }) {
+  return (
+    <div className="card col-12 col-md-6 col-lg-4 mb-4" style={{ width: '18rem' }} key={card.id}>
+      <img src="..." class="card-img-top" alt="..."></img>
+      <div className="card-body">
+        <h5 className="card-title">{card.name}</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <div className="card-body">
+        <a href={`/stars/${card.id}`} className="card-link">Card link</a>
+        <a href="#" className="card-link">Распознать текст</a>
+        <a href="#" className="card-link">Воспроизвести текст</a>
+      </div>
+    </div>
+  );
+};
--- views/NavMenu.jsx
+++ views/NavMenu.jsx
@@ -5,7 +5,7 @@ module.exports = function NavMenu({ user }) {
     <header className="header">
       <nav className="navbar bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand">Granny Helper</a>
+          <a className="navbar-brand">Бабушка Helper</a>
           <form className="d-flex" role="search">
             {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" /> */}
             <button className="btn btn-outline-success" type="submit">Выйти</button>
