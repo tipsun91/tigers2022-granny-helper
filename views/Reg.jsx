9ff353621609e29719107af03b21192a0113e931
tipsun91 <tipsun911@gmail.com>
2022-07-07 19:30:34 +0300

    Fixed form names

--- views/Reg.jsx
+++ views/Reg.jsx
@@ -52,21 +52,21 @@ module.exports = function Reg({ errors }) {
             <br />
             <label className='reg_content reg_text' htmlFor="name_input">Имя:</label>
             <br />
-            <input className='reg_content reg_input' id="name_input" name="user[name]" type="text" value="" />
+            <input className='reg_content reg_input' id="name_input" name="name" type="text" value="" />
 
             <div className="form-check">
               <label className="reg_content reg_text" for="flexRadioDefault1">Бабушка: </label>
-              <input className="reg_content reg_input_radio" type="radio" name="user[role]" value="gm" id="flexRadioDefault1" checked />
+              <input className="reg_content reg_input_radio" type="radio" name="role" value="gm" id="flexRadioDefault1" checked />
             </div>
 
             <div className="form-check">
               <label className="reg_content reg_text" for="flexRadioDefault2">Внук: </label>
-              <input className="reg_content reg_input_radio" type="radio" name="user[role]" value="gs" id="flexRadioDefault2" />
+              <input className="reg_content reg_input_radio" type="radio" name="role" value="gs" id="flexRadioDefault2" />
             </div>
 
             <label className="reg_content reg_text" htmlFor="password_input">Пароль:</label>
             <br />
-            <input className="reg_content reg_input" id="password_input" name="user[password]" type="password" value="" />
+            <input className="reg_content reg_input" id="password_input" name="password" type="password" value="" />
             <br />
             <button className="reg_content btn btn-primary" type="submit" value="Зарегистрироваться">Зарегистрироваться</button>
             {/* <input className="reg_content btn btn-primary" type="submit" value="Зарегистрироваться" /> */}
