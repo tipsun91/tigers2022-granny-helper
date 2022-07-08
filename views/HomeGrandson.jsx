18ad0bf04739740d72573f4bd3ec10650826086b
Nikita Krivosheyev <nik.krivosheyev@gmail.com>
2022-07-08 11:22:57 +0300

    grandson`s home added

---
+++ views/HomeGrandson.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable jsx-a11y/aria-role */
+const React = require('react');
+const NavMenu = require('./NavMenu');
+const Layout = require('./Layout');
+
+module.exports = function HomeGranson({ text, user }) {
+  return (
+    <Layout>
+      <NavMenu user={user} />
+      <h1>Картинки</h1>
+
+    </Layout>
+  );
+};
