63d0ac99245bcfc75af74ac580b89519085e3fdd
Nikita Krivosheyev <nik.krivosheyev@gmail.com>
2022-07-08 11:23:40 +0300

    home routes added

---
+++ routes/home.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const Home = require('../views/Home');
+
+router.get('/', async (req, res) => {
+  res.renderComponent(Home, { title: 'Home' });
+});
+
+module.exports = router;
